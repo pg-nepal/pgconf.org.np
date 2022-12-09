4549cd6c03ddc4ef3fed6ea04a6e11838141ffdd
rughimire <rughimire@gmail.com>
2022-12-09 20:07:46 +0545

    flask templating + content pages added

---
+++ run.py
@@ -0,0 +1,21 @@
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def render_home():
+    return flask.render_template(
+        'home.html'
+    )
+
+
+@app.route('/<page>')
+def render_page(page):
+    return flask.render_template(
+        page + '.html'
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
