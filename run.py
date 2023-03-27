00c94a8d9e0eb1bb345c8320be64a5e9fdde5c8a
rughimire <rughimire@gmail.com>
2023-03-27 19:00:44 +0545

    speaker and talk related page design

--- run.py
+++ run.py
@@ -29,5 +29,33 @@ def render_page(page):
     )
 
 
+@app.route('/speakers/all')
+def render_speaker_page():
+    from talks import keynotes,talks, workshops
+    return flask.render_template(
+        'speakers.djhtml',
+        keynotes  = keynotes,
+        talks     = talks,
+        workshops = workshops    
+    )
+
+
+@app.route('/speaker/<slug>')
+def render_speaker_page_slug(slug):
+    from talks import keynotes,talks, workshops
+
+    talks_list = keynotes + talks + workshops
+    selected_talk = None
+
+    for talk in talks_list:
+        if talk['slug'] != slug: continue
+        selected_talk = talk
+
+    return flask.render_template(
+        'speakers_indivisual.djhtml', 
+        talk     = selected_talk
+    )
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
---
+++ talks.py
@@ -0,0 +1,37 @@
+keynotes = [ 
+    {
+        'type'    : 'Keynote',
+        'slug'    : 'keynote_1',
+        'image'   : 'oleg.jpg',
+        'speaker' : 'Oleg Bartunov',
+        'title'   : 'Postgres Innovations',
+        'short_bio' : "Oleg Bartunov has been involved in PostgreSQL development since 1996 (he introduced locale support). He is a major developer and a member of the PGDG. He is the major contributor for ser-define index access methods GiST, GIN and SP-GiST, built-in full-text search facilities in PostgreSQL and a number of popular extensions like intarray, ltree, hstore, pg_trgm, jsquery and rum access method. His latest contributions are jsonb and implementation of SQL/JSON.",
+        'long_bio'  : """
+                            Oleg Bartunov has been involved in PostgreSQL development since 1996 (he introduced locale support). He is a major developer and a member of the PGDG. Together with his colleague Teodor Sigaev he developed infrastructure for implementing user-define index access methods GiST, GIN and SP-GiST, built-in full-text search facilities in PostgreSQL (formerly known as tsearch2) and a number of popular extensions like intarray, ltree, hstore, pg_trgm, jsquery and rum access method. His latest contributions are jsonb and implementation of SQL/JSON. His current project is pluggable TOAST API and  scalable JSONB/JSON.
+
+                            Oleg graduated from the Astronomy department of the Physics department at Lomonosov's Moscow State University. During his professional work at Sternberg Astronomical Institute (SAI MSU) he realized he needed a free and powerful open-source database like PostgreSQL (Postgres95 that time). Since then he extensively used PostgreSQL in his scientific work and many other projects. Oleg is an active member of the Russian PostgreSQL community, he advocated the adoption of PostgreSQL by the astronomical community.
+                        """,
+        'abstract'  : """
+                       PostgreSQL, commonly referred to as Postgres, is a free and open-source relational database management system that has been in development for over 30 years. This talk aims to delve into the innovations that have been introduced in Postgres over the years, including the initial design of Postgres, the historical background of some innovative features, and the future horizons of Postgres.
+                        """
+    },
+    {
+        'type'    : 'Keynote',
+        'slug'    : 'keynote_2',
+        'image'   : 'chris.jpg',
+        'speaker' : 'Chris Travers',
+        'title'   : 'What Database Teams Can Learn from Flight Safety?',
+        'short_bio' : "Chris has 24 years of experience with PostgreSQL as an application developer, database administrator, and database engineer.  He has also worked as an engineering manager for teams maintaining critical environments.  Chris's teams have managed PostgreSQL environments into the petabytes, and he has significant experience with mission-critical operations both as an individual contributor and as a manager. ",
+        'long_bio'  : """
+                            Chris has 24 years of experience with PostgreSQL as an application developer, database administrator, and database engineer.  He has also worked as an engineering manager for teams maintaining critical environments.  Chris's teams have managed PostgreSQL environments into the petabytes, and he has significant experience with mission-critical operations both as an individual contributor and as a manager.  One particularly important contribution was the implementation of human factors or crew resource management training at Adjust, where he used to work.  Chris has become a strong believer that our industry shortcomings can be resolved by looking at how other critical industries, such as airlines, avoid and manage incidents.
+                            
+                        """,
+        'abstract'  : """
+                            Sixty years ago, the airline industry blamed 90% of aircraft accidents on pilot error.  Today the percentage is similar, and yet flying is nearly a hundred times safer.  This means that the aviation industry has become better at preventing human error from causing accidents as quickly as they have become better at preventing and mitigating mechanical and technical faults.  Yet, our industry still makes very little effort to understand the kinds of mistakes people make or why.   This is a talk about how to dramatically reduce human error in database operations by leveraging the lessons from aviation based on experience implementing such programs.  In this program we will discuss some common causes of human error and how we are often complicit in making that more likely, as well as concrete steps we need to take as an industry to make that less likely, as well as how to go about trying to implement such change.
+                        """
+    },
+]
+
+workshops = []
+
+talks = []
