b4caccea1d61f8b0a3f139d1dd3c74f0700de394
chimpKiller <shashankshrestha21@gmail.com>
2024-12-17 12:56:53 +0545

    proposals - crud.py

---
+++ srv/proposals/crud.py
@@ -0,0 +1,64 @@
+import flask
+import sqlalchemy as sa
+
+import db
+import db.proposals
+
+from srv import app
+
+
+@app.get('/proposals/form')
+def proposal_form():
+    return flask.render_template(
+        '/proposals/form.djhtml',
+    )
+
+
+@app.get('/proposals')
+def proposal_list():
+    query = sa.select(db.proposals.Proposals)
+
+    with db.engine.connect() as connection:
+        cursor = connection.execute(query)
+
+    return flask.render_template(
+        '/proposals/list.djhtml',
+        cursor = cursor,
+    )
+
+
+@app.post('/proposals/add')
+def proposal_create():
+    formData = flask.request.form.to_dict()
+
+    query = sa.insert(
+        db.proposals.Proposals,
+    ).values(
+        **formData,
+    )
+
+    with db.SessionMaker.begin() as session:
+        session.execute(query)
+
+    return flask.redirect('/pages/call-for-proposal')
+
+
+@app.get('/proposals/<int:pk>')
+def proposal_read(pk):
+    query = sa.select(
+        db.proposals.Proposals,
+    ).where(
+        db.proposals.Proposals.pk == pk,
+    )
+
+    with db.engine.connect() as connection:
+        cursor = connection.execute(query)
+        row = cursor.first()
+
+    if row is None:
+        return 'Invalid Pk', 400
+
+    return flask.render_template(
+        '/proposals/read.djhtml',
+        proposal = row._asdict(),
+    )
