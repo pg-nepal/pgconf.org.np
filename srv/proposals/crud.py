4941740355f5ddfe7cd40169738b05e2aeb87b2e
chimpKiller <shashankshrestha21@gmail.com>
2024-12-17 12:56:53 +0545

    proposals - crud.py

---
+++ srv/proposals/crud.py
@@ -0,0 +1,35 @@
+import flask
+import sqlalchemy as sa
+
+import db
+import db.programs
+import srv.auth
+
+from srv import app
+
+
+@app.get('/proposals/form')
+def proposal_page_form():
+    return flask.render_template(
+        '/proposals/form.djhtml',
+    )
+
+
+@app.post('/api/proposals/add')
+def proposal_create():
+    isAdmin = srv.auth.isValid(flask.request)
+    if isAdmin is False:
+        return srv.auth.respondInValid()
+
+    formData = flask.request.form
+
+    query = sa.insert(
+        db.programs.Proposal,
+    ).values(
+        **formData,
+        createdBy = isAdmin,
+    )
+
+    with db.SessionMaker.begin() as session:
+        session.execute(query)
+        return flask.redirect('/pages/call-for-proposal')
