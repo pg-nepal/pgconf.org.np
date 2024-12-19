bbfe509f2f7d354d46ed0609dfa0df7a10decbec
rho <rho.rhoit@gmail.com>
2024-12-19 11:58:50 +0545

    run.py

---
+++ run.py
@@ -0,0 +1,26 @@
+#!venv/bin/python
+
+import datetime as dt
+
+import srv.pages
+import srv.proposals
+
+from srv import app
+
+
+eventOn = dt.datetime(2025, 5, 5)
+eventTo = dt.datetime(2025, 5, 6)
+
+
+app.jinja_env.globals.update({
+    'eventOn' : eventOn,
+    'eventTo' : eventTo,
+})
+
+
+if __name__ == '__main__':
+    app.run(
+        host  = '0.0.0.0',
+        port  = 5000,
+        debug = True,
+    )
