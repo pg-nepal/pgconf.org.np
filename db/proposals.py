eeee17bd049263c5caa7ab842b8d166c7ce854df
rho <rho.rhoit@gmail.com>
2025-01-03 17:12:11 +0545

    db - proposals.py

---
+++ db/proposals.py
@@ -0,0 +1,24 @@
+import sqlalchemy as sa
+
+import db
+
+
+class Proposals(db.Base):
+    __tablename__  = 'proposals'
+    __table_args__ = {
+        'schema'  : 'conf25',
+        'comment' : 'conference proposals',
+    }
+
+    pk             = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+
+    name           = sa.Column(sa.String(256), nullable=False)
+    email          = sa.Column(sa.String(256))
+    address        = sa.Column(sa.String(256))
+    country        = sa.Column(sa.String(256))
+    phone          = sa.Column(sa.String(10))
+
+    # Session Details
+    type           = sa.Column(sa.String(50))
+    title          = sa.Column(sa.String(100))
+    abstract       = sa.Column(sa.String(800))
