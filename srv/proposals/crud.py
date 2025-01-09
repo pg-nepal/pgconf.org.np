97c32ceb6a8f86d3038757bf2f27dd43404f753c
chimpKiller <shashankshrestha21@gmail.com>
2025-01-09 16:13:15 +0545

    formatted properly

--- srv/proposals/crud.py
+++ srv/proposals/crud.py
@@ -1,22 +1,22 @@
+import db
+import db.proposals
 import flask
 import sqlalchemy as sa
 
-import db
-import db.proposals
 import srv.auth
-
 from srv import app
+
 # from flask_jwt_extended import jwt_required, get_jwt_identity
 
 
-@app.get('/proposals/form')
+@app.get("/proposals/form")
 def proposal_form():
     return flask.render_template(
-        '/proposals/form.djhtml',
+        "/proposals/form.djhtml",
     )
 
 
-@app.get('/proposals')
+@app.get("/proposals")
 def proposal_list():
     isAdmin = srv.auth.isValid(flask.request)
     if isAdmin is False:
@@ -28,30 +28,26 @@ def proposal_list():
         cursor = connection.execute(query)
 
     return flask.render_template(
-        '/proposals/list.djhtml',
-        cursor  = cursor,
-        isAdmin = isAdmin,
+        "/proposals/list.djhtml",
+        cursor=cursor,
+        isAdmin=isAdmin,
     )
 
 
-@app.post('/proposals/add')
+@app.post("/proposals/add")
 # @jwt_required()
 def proposal_create():
     formdata = flask.request.form.to_dict()
 
-    query = sa.insert(
-        db.proposals.Proposals
-    ).values(
-        **formdata
-    )
+    query = sa.insert(db.proposals.Proposals).values(**formdata)
 
     with db.Session.begin() as session:
         session.execute(query)
 
-    return flask.redirect('/call_for_proposal')
+    return flask.redirect("/call_for_proposal")
 
 
-@app.get('/proposals/<int:pk>')
+@app.get("/proposals/<int:pk>")
 def proposal_read(pk):
     isAdmin = srv.auth.isValid(flask.request)
     if isAdmin is False:
@@ -68,24 +64,24 @@ def proposal_read(pk):
         row = cursor.first()
 
     if row is None:
-        return 'Invalid Pk', 400
+        return "Invalid Pk", 400
 
     return flask.render_template(
-        '/proposals/read.djhtml',
-        proposal = row._asdict(),
-        isAdmin  = isAdmin,
+        "/proposals/read.djhtml",
+        proposal=row._asdict(),
+        isAdmin=isAdmin,
     )
 
 
-@app.delete('/proposals/<int:pk>')
+@app.delete("/proposals/<int:pk>")
 def proposal_delete(pk):
     with db.Session.begin() as session:
-        session.execute(sa.delete(
-            db.proposals.Proposals,
-        ).where(
-            db.proposals.Proposals.pk==pk,
-        ))
+        session.execute(
+            sa.delete(
+                db.proposals.Proposals,
+            ).where(
+                db.proposals.Proposals.pk == pk,
+            )
+        )
 
-    return flask.jsonify({
-        'message' : 'Proposal deleted successfully'
-    }), 202
+    return flask.jsonify({"message": "Proposal deleted successfully"}), 202
