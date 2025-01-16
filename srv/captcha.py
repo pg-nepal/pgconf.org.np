94e6508a45b47c3c80fb6bca0192134b6b91d50d
chimpKiller <shashankshrestha21@gmail.com>
2025-01-16 16:02:34 +0545

    captcha.py

---
+++ srv/captcha.py
@@ -0,0 +1,7 @@
+questions = [
+    ('What is used to retrieve data from a database?', 'SELECT'),
+    ('What is used to remove data from a table?', 'DELETE'),
+    ('What command is used to modify data in a table?', 'UPDATE'),
+    ('What is used to sort data in SQL?', 'ORDER'),
+    ('What SQL keyword is used to combine results from two or more tables?', 'JOIN'),  # noqa:E501
+]
