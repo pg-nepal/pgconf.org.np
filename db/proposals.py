e5982cf97b0ea6bfa179a3af12880605ec08d8af
chimpKiller <shashankshrestha21@gmail.com>
2025-01-19 16:35:47 +0545

    added session id and status to the proposal table

--- db/proposals.py
+++ db/proposals.py
@@ -1,4 +1,5 @@
 import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import ENUM
 
 import db
 
@@ -11,6 +12,12 @@ class Proposals(db.Base):
     }
 
     pk             = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+    session_id     = sa.Column(sa.String(36))
+    status         = sa.Column(
+                  ENUM('accepted', 'pending', 'rejected', name='proposal_status'),
+                  nullable=False,
+                  server_default='pending'
+    )
 
     name           = sa.Column(sa.String(256), nullable=False)
     email          = sa.Column(sa.String(256))
