f5d45f1ee829b52e7b9392bc0ca8aa491c82a5b7
rho <rho.rhoit@gmail.com>
2025-01-23 23:30:10 +0545

    Revert "__init__ - added jwt"
    This reverts commit 0d4727dbc9d5a6b0e9a5fb9f31f56cdb78e78cb0.

--- srv/__init__.py
+++ srv/__init__.py
@@ -1,24 +1,9 @@
-import os
-import secrets
-import datetime as dt
-
 import flask
 import jinja2
-from flask_jwt_extended import JWTManager
 
-
-DURATION  = dt.timedelta(hours=1)
-SECRET_KEY = os.getenv('SECRET_KEY', secrets.token_bytes(16).hex())
-print(SECRET_KEY)
 
 app = flask.Flask(__name__)
 
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 app.jinja_env.undefined = jinja2.StrictUndefined
-app.secret_key = SECRET_KEY
-
-app.config['JWT_SECRET_KEY'] = SECRET_KEY
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = DURATION
-
-jwt = JWTManager(app)
