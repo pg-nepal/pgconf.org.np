6ae1cd7292cd9a451b4b94c2874c86984e5953a2
chimpKiller <shashankshrestha21@gmail.com>
2025-01-26 02:03:21 +0545

    attendees - registration.py CREATE

--- srv/attendees/registration.py
+++ srv/attendees/registration.py
@@ -8,6 +8,39 @@ import uploads
 from srv import app
 
 
+@app.post('/registered/add')
+def registered_create():
+    formData = flask.request.form
+
+    category = formData.get('category')
+    with db.engine.connect() as connection:
+        cursor = connection.execute(sa.select(
+            sa.func.count().label('count'),
+        ).where(
+            db.events.Attendee.category == category,
+        ))
+
+        count = cursor.scalar()
+
+    discount_student = 2000 if category == 'student' else 0
+    discount_early = 2000 if count < 20 else 0
+
+    query = sa.insert(
+        db.events.Attendee,
+    ).values(
+        **formData,
+        fee    = 7000 - discount_student - discount_early,
+        status = 'pending',
+        ticket = 'ticket',
+    ).returning(
+        db.events.Attendee.slug,
+    )
+
+    with db.SessionMaker.begin() as session:
+        cursor = session.execute(query)
+        return flask.redirect('/registered/{}'.format(cursor.scalar()))
+
+
 @app.get('/registered/<slug>')
 def registered_read(slug):
     query = sa.select(
