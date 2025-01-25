8fa31cec0048a97405f548b7464cbff0c821b09e
chimpKiller <shashankshrestha21@gmail.com>
2025-01-26 01:58:07 +0545

    attendees - registration.py update

--- srv/attendees/registration.py
+++ srv/attendees/registration.py
@@ -3,12 +3,13 @@ import sqlalchemy as sa
 
 import db
 import db.events
+import uploads
 
 from srv import app
 
 
-@app.get('/attendees/<slug>')
-def attendee_read_client(slug):
+@app.get('/registered/<slug>')
+def registered_read_client(slug):
     query = sa.select(
         db.events.Attendee,
     ).where(
@@ -31,3 +32,22 @@ def attendee_read_client(slug):
             '/attendees/profile.djhtml',
             row = data,
         )
+
+
+@app.post('/registered/<slug>')
+def registered_update(slug):
+    fileData = flask.request.files
+
+    fullpath = uploads.save(fileData, 'receipt_url')
+
+    query = sa.update(
+        db.events.Attendee,
+    ).where(
+        db.events.Attendee.slug == slug.encode(),
+    ).values(
+        receipt_url = fullpath,
+    )
+
+    with db.SessionMaker.begin() as session:
+        cursor = session.execute(query)
+        return 'Updated Rows', 202 if cursor.rowcount > 0 else 400
