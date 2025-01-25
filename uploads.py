c5f0575c7ddd0ba59e9b4ca91c098f59f39f2495
chimpKiller <shashankshrestha21@gmail.com>
2025-01-26 01:58:07 +0545

    attendees - registration.py update

--- uploads.py
+++ uploads.py
@@ -10,5 +10,16 @@ PATH_identification = PATH_base / 'identification'
 PATH_receipts = PATH_base / 'receipts'
 
 
-def allowed_file(filename):
-    return filename.endswith(('pdf', 'jpg', 'png'))
+def save(fileData, field):
+    fobj = fileData.get()
+
+    if fobj is None:
+        return
+
+    filename = fobj.filename
+    if filename.endswith(('pdf', 'jpg', 'png')):
+        name = werkzeug.utils.secure_filename(filename)
+        fullpath = PATH_receipts / name
+        fobj(fullpath)
+        fullpath.parent.mkdir(parents=True, exist_ok=True)
+        return fullpath
