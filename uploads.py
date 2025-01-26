5f187bc2e1f7a7d927be1e354361dfcbddd49a3f
chimpKiller <shashankshrestha21@gmail.com>
2025-01-26 12:20:38 +0545

    uploads.py save

--- uploads.py
+++ uploads.py
@@ -9,6 +9,21 @@ from srv import app
 PATH_base = pathlib.Path('./uploads/attendees')
 
 
+def save(fileData, field):
+    fobj = fileData.get()
+
+    if fobj is None:
+        return
+
+    filename = fobj.filename
+    if filename.endswith(('pdf', 'jpg', 'png')):
+        name = werkzeug.utils.secure_filename(filename)
+        fullpath = PATH_base / name
+        fobj(fullpath)
+        fullpath.parent.mkdir(parents=True, exist_ok=True)
+        return str(fullpath)
+
+
 @app.route('/uploads/attendees/<filename>')
 def file_srv(filename):
     return flask.send_from_directory(PATH_base, filename)
