c3bf7592370282468015f0de5e63ae8475f1a4cf
chimpKiller <shashankshrestha21@gmail.com>
2025-02-04 17:08:59 +0545

    crud - route for listing all the reviews

--- srv/reviews/crud.py
+++ srv/reviews/crud.py
@@ -7,3 +7,21 @@ import srv.captcha
 import srv.auth
 
 from srv import app
+
+@app.get('/reviews')
+def review_list():
+    isAdmin = srv.auth.isValid(flask.request)
+    if isAdmin is False:
+        return srv.auth.respondInValid()
+
+    query = sa.select(db.proposals.Review)
+
+    with db.engine.connect() as connection:
+        cursor = connection.execute(query)
+        review = [row._asdict() for row in cursor]
+
+    return flask.jsonify({
+        "review" : review,
+        "isAdmin" : isAdmin,
+        })
+
