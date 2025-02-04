a4a7bdd7166e9d2504eaa94b092b80a1b412fe6c
chimpKiller <shashankshrestha21@gmail.com>
2025-02-04 10:22:39 +0545

    crud - Add endpoint to fetch and display a single proposal with associated reviews

---
+++ srv/reviews/api.py
@@ -0,0 +1,27 @@
+import flask
+import sqlalchemy as sa
+
+import db
+import db.proposals
+import srv.auth
+
+from srv import app
+
+
+@app.get('/api/proposals/review/<int:pk>')
+def proposal_read(pk):
+    isAdmin = srv.auth.isValid(flask.request)
+    if isAdmin is False:
+        return srv.auth.respondInValid()
+
+    query = sa.select(
+        db.proposals.Review,
+    ).where(
+        db.proposals.Review.proposal_pk == pk,
+    )
+
+    with db.engine.connect() as connection:
+        cursor = connection.execute(query)
+        return flask.jsonify(
+            [ row._asdict() for row in cursor ],
+        )
