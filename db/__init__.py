101b7af253c3e0b1d50fdd9327d017976c35211b
chimpKiller <shashankshrestha21@gmail.com>
2025-02-10 15:33:38 +0545

    __init__ - imported datetime as dt for setting the default for createdon and updatedon as the system's current local time

--- db/__init__.py
+++ db/__init__.py
@@ -3,6 +3,7 @@
 import os
 import sqlalchemy as sa
 import sqlalchemy.orm
+import datetime as dt
 
 
 url = sa.URL.create(
@@ -29,7 +30,7 @@ meta = sa.MetaData()
 class Base(sqlalchemy.orm.DeclarativeBase):
     metadata  = meta
 
-    createdOn = sa.Column(sa.DateTime(timezone=True), nullable=False, server_default=sa.func.now())  # noqa:E501
+    createdOn = sa.Column(sa.DateTime(timezone=True), nullable=False, default=dt.datetime.now)  # noqa:E501
     createdBy = sa.Column(sa.String(256))
-    updatedOn = sa.Column(sa.DateTime(timezone=True), nullable=False, server_default=sa.func.now(), onupdate=sa.func.now())  # noqa:E501
+    updatedOn = sa.Column(sa.DateTime(timezone=True), nullable=False, server_default=sa.func.now(), onupdate=dt.datetime.now)  # noqa:E501
     updatedBy = sa.Column(sa.String(32))
