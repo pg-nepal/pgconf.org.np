dec67b8d3025d657fa3b07bf27c5a51409ad8fe8
chimpKiller <shashankshrestha21@gmail.com>
2025-02-12 15:58:47 +0545

    uploads - made PATH_base an absolute path using .resolve

--- uploads.py
+++ uploads.py
@@ -6,7 +6,7 @@ import werkzeug.utils
 from srv import app
 
 
-PATH_base = pathlib.Path('./srv/uploads/receipts')
+PATH_base = pathlib.Path('./srv/uploads/receipts').resolve()
 
 
 def save(fileData, field):
