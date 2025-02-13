1b4430bc689c82cb2acb61b086c8a009bf203cf8
chimpKiller <shashankshrestha21@gmail.com>
2025-02-13 14:47:36 +0545

    feature - used sha256 hash instead of md5 and minor changes

--- srv/registrations/feature.py
+++ srv/registrations/feature.py
@@ -5,9 +5,8 @@ import hashlib
 def generate_registration_id(name, email):
     timestamp = str(int(datetime.utcnow().timestamp()))
     hash_input = f"{name} {email} {timestamp}"
-    unique_hash = hashlib.md5(hash_input.encode()).hexdigest()[:6]
-    registration_id = f"REG-{unique_hash.upper()}"
-    return registration_id
+    unique_hash = hashlib.sha256(hash_input.encode()).hexdigest()[:6]
+    return f"REG-{unique_hash.upper()}"
 
 def set_generate_registration_id(self, registration_id):
     self.registration_id = registration_id
