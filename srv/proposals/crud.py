c89cce5757dadc650a20ba82e248b48948229e97
chimpKiller <shashankshrestha21@gmail.com>
2025-02-13 16:01:51 +0545

    crud - shifted the route for proposal_list_api to api file

--- srv/proposals/crud.py
+++ srv/proposals/crud.py
@@ -34,42 +34,6 @@ def proposal_list():
     )
 
 
-@app.get('/api/proposals')
-def proposal_list_api():
-    isAdmin = srv.auth.isValid(flask.request)
-    if isAdmin is False:
-        return srv.auth.respondInValid()
-
-    query = (
-        sa.select(
-            db.proposals.Proposal.pk,
-            db.proposals.Proposal.title,
-            sa.func.coalesce(
-                sa.func.round(sa.func.avg(db.proposals.Rating.rating), 0),
-            ).label('avg(rating)'),
-            db.proposals.Proposal.category,
-            db.proposals.Proposal.createdOn,
-        )
-        .outerjoin(
-            db.proposals.Rating,
-            db.proposals.Proposal.pk == db.proposals.Rating.proposal_pk,
-        )
-        .group_by(
-            db.proposals.Proposal.pk,
-        )
-        .order_by(
-            sa.desc('avg(rating)'),
-        )
-    )
-
-    with db.engine.connect() as connection:
-        cursor = connection.execute(query)
-        return flask.jsonify(
-            headers=tuple(c['name'] for c in query.column_descriptions),
-            data=[list(row) for row in cursor],
-        )
-
-
 @app.post('/proposals/add')
 def proposal_create():
     formData = flask.request.form.to_dict()
