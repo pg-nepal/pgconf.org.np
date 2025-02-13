7d42cae03faa3beee1a01bf5966d307981dfe67f
rho <rho.rhoit@gmail.com>
2025-02-13 11:11:54 +0545

    table.mjs

---
+++ srv/static/table.mjs
@@ -0,0 +1,48 @@
+export function load(id, baseURL) {
+    const api = `/api${baseURL}`
+    fetch(api).then(function (response) {
+        if (!response.ok) {
+            alert(`${response.status} ${response.statusText} `)
+        }
+        return response.json()
+    }).then(function (json) {
+        const eTable = document.getElementById(id)
+
+        for (const h of json.headers) {
+            const eTh = document.createElement('th')
+            eTh.innerHTML = h
+            eTable.children[0].children[0].append(eTh)
+        }
+
+        for (const row of json.data) {
+            const eTr = document.createElement('tr')
+
+
+            for (const [i, v] of Object.entries(row)) {
+                const eTd = document.createElement('td')
+
+                switch (json.headers[i]) {
+                    case 'pk':
+                        const eA = document.createElement('a')
+                        eA.innerText = v
+                        eA.href = `${baseURL}/${v}`
+                        eTd.append(eA)
+                        break
+
+                    case 'createdOn':
+                        if (null == v) break
+                        const date = new Date(v)
+                        eTd.innerHTML = date.toString().substring(0, 24)
+                        break
+
+                    default:
+                        eTd.innerHTML = v
+
+                }
+                eTr.append(eTd)
+            }
+
+            eTable.children[1].append(eTr)
+        }
+    })
+}
