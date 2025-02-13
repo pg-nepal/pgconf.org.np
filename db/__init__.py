e2475cba1d6ce43fde231258460f0d409114218a
chimpKiller <shashankshrestha21@gmail.com>
2025-02-13 14:35:09 +0545

    __init__ - removed untriggerd # noqa

--- db/__init__.py
+++ db/__init__.py
@@ -1,25 +1,25 @@
 # flake8: noqa:F401
 
+import datetime as dt
 import os
+
 import sqlalchemy as sa
 import sqlalchemy.orm
-import datetime as dt
-
 
 url = sa.URL.create(
     'postgresql+psycopg',
-    username = os.getenv('PG_USER'),
-    password = os.getenv('PG_PASSWORD'),
-    port     = os.getenv('PG_PORT'),
-    host     = os.getenv('PG_HOST', 'localhost'),
-    database = os.getenv('PG_DB', 'pug'),
+    username=os.getenv('PG_USER'),
+    password=os.getenv('PG_PASSWORD'),
+    port=os.getenv('PG_PORT'),
+    host=os.getenv('PG_HOST', 'localhost'),
+    database=os.getenv('PG_DB', 'pug'),
 )
 
 isDebug = bool(os.getenv('DEBUG'))  # direct access
 engine = sa.create_engine(
-    url    = url,
-    future = True,
-    echo   = isDebug,  # default:False
+    url=url,
+    future=True,
+    echo=isDebug,  # default:False
 )
 
 SessionMaker = sa.orm.sessionmaker(bind=engine)
@@ -28,9 +28,9 @@ meta = sa.MetaData()
 
 
 class Base(sqlalchemy.orm.DeclarativeBase):
-    metadata  = meta
+    metadata = meta
 
-    createdOn = sa.Column(sa.DateTime(timezone=True), nullable=False)  # noqa:E501
+    createdOn = sa.Column(sa.DateTime(timezone=True), nullable=False)
     createdBy = sa.Column(sa.String(256))
-    updatedOn = sa.Column(sa.DateTime(timezone=True), nullable=False)  # noqa:E501
+    updatedOn = sa.Column(sa.DateTime(timezone=True), nullable=False)
     updatedBy = sa.Column(sa.String(32))
