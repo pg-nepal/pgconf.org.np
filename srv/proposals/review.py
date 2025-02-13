9fd1e896830a519258e1a459b94c5574095b0056
chimpKiller <shashankshrestha21@gmail.com>
2025-02-13 14:46:17 +0545

    registration - saved in correct format

--- srv/proposals/review.py
+++ srv/proposals/review.py
@@ -4,7 +4,6 @@ import sqlalchemy as sa
 import db
 import db.proposals
 import srv.auth
-
 from srv import app
 
 
@@ -14,21 +13,31 @@ def proposal_list_review():
     if isAdmin is False:
         return srv.auth.respondInValid()
 
-    query = sa.select(
-        db.proposals.Proposal,
-        sa.func.coalesce(sa.func.round(sa.func.avg(db.proposals.Review.rating), 0)).label('avg(rating)'),
-    ).outerjoin(
-        db.proposals.Review, db.proposals.Proposal.pk == db.proposals.Review.proposal_pk,
-    ).group_by(
-        db.proposals.Proposal.pk,
-    ).order_by(
-        sa.desc('avg(rating)').nulls_last(),
+    query = (
+        sa.select(
+            db.proposals.Proposal,
+            sa.func.coalesce(
+                sa.func.round(sa.func.avg(db.proposals.Review.rating), 0),
+            ).label('avg(rating)'),
+        )
+        .outerjoin(
+            db.proposals.Review,
+            db.proposals.Proposal.pk == db.proposals.Review.proposal_pk,
+        )
+        .group_by(
+            db.proposals.Proposal.pk,
+        )
+        .order_by(
+            sa.desc('avg(rating)').nulls_last(),
+        )
     )
 
     with db.engine.connect() as connection:
         cursor = connection.execute(query)
+        result = cursor.fetchall()
+        print('Bing:', result)
 
     return flask.render_template(
-        '/proposals/list.djhtml',
-        cursor  = cursor,
+        '/proposals/read.djhtml',
+        cursor=result,
     )
