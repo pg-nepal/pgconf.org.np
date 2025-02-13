8765151dd5635d531579b505aae9646bbceb8763
chimpKiller <shashankshrestha21@gmail.com>
2025-02-13 14:45:55 +0545

    registration - saved in correct format

--- srv/attendees/registration.py
+++ srv/attendees/registration.py
@@ -7,7 +7,6 @@ import db
 import db.events
 import srv.captcha
 import uploads
-
 from srv import app
 
 
@@ -16,8 +15,8 @@ def registered_form():
     idx = random.randrange(len(srv.captcha.questions))  # noqa:S311
     return flask.render_template(
         '/attendees/form-registration.djhtml',
-        idx      = idx,
-        question = srv.captcha.questions[idx][0],
+        idx=idx,
+        question=srv.captcha.questions[idx][0],
     )
 
 
@@ -33,39 +32,49 @@ def registered_create():
 
     category = formData.get('category')
     with db.engine.connect() as connection:
-        cursor = connection.execute(sa.select(
-            sa.func.count().label('count'),
-        ).where(
-            db.events.Attendee.category == category,
-        ))
+        cursor = connection.execute(
+            sa.select(
+                sa.func.count().label('count'),
+            ).where(
+                db.events.Attendee.category == category,
+            ),
+        )
 
         count = cursor.scalar()
 
     discount_student = 2000 if category == 'student' else 0
     discount_early = 2000 if count < 20 else 0
 
-    query = sa.insert(
-        db.events.Attendee,
-    ).values(
-        **formData,
-        fee    = 7000 - discount_student - discount_early,
-        status = 'pending',
-        ticket = 'ticket',
-    ).returning(
-        db.events.Attendee.slug,
+    query = (
+        sa.insert(
+            db.events.Attendee,
+        )
+        .values(
+            **formData,
+            fee=7000 - discount_student - discount_early,
+            status='pending',
+            ticket='ticket',
+        )
+        .returning(
+            db.events.Attendee.slug,
+        )
     )
 
     try:
         with db.SessionMaker.begin() as session:
             cursor = session.execute(query)
-            return flask.jsonify({
-                'redirect': '/registered/{}'.format(cursor.scalar()),
-            }), 201
+            return flask.jsonify(
+                {
+                    'redirect': '/registered/{}'.format(cursor.scalar()),
+                },
+            ), 201
 
     except Exception:
-        return flask.jsonify({
-            'error': 'Registration with the following email already exists', # noqa: E501
-        }), 400
+        return flask.jsonify(
+            {
+                'error': 'Registration with the following email already exists',  # noqa: E501
+            },
+        ), 400
 
 
 @app.get('/registered/<slug>')
@@ -91,7 +100,7 @@ def registered_read_client(slug):
 
         return flask.render_template(
             '/attendees/profile.djhtml',
-            row = row._asdict(),
+            row=row._asdict(),
         )
 
 
@@ -101,12 +110,16 @@ def registered_update(slug):
 
     fullpath = uploads.save(fileData, 'receiptPath')
 
-    query = sa.update(
-        db.events.Attendee,
-    ).where(
+    query = (
+        sa.update(
+            db.events.Attendee,
+        )
+        .where(
             sa.cast(db.events.Attendee.slug, sa.String) == slug,
-    ).values(
-        receiptPath = fullpath,
+        )
+        .values(
+            receiptPath=fullpath,
+        )
     )
 
     with db.SessionMaker.begin() as session:
