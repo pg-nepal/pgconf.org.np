b06b46f425e5eb478e89e75c93ad384f8eb612ed
rho <rho.rhoit@gmail.com>
2025-02-14 17:14:11 +0545

    attendees - registration.py duplicate registration

--- srv/attendees/registration.py
+++ srv/attendees/registration.py
@@ -1,6 +1,8 @@
 import random
+import traceback
 
 import flask
+import psycopg.errors
 import sqlalchemy as sa
 
 import db
@@ -55,9 +57,15 @@ def registered_create():
         db.events.Attendee.slug,
     )
 
-    with db.SessionMaker.begin() as session:
-        cursor = session.execute(query)
-        return flask.redirect('/registered/{}'.format(cursor.scalar()))
+    try:
+        with db.SessionMaker.begin() as session:
+            cursor = session.execute(query)
+            # not returning 201 because of redirection
+            return flask.redirect('/registered/{}'.format(cursor.scalar()))
+    except sa.exc.IntegrityError as e:
+        if not isinstance(e, psycopg.errors.UniqueViolation):
+            return 'email in used has already been registered', 400
+        traceback.print_exc()
 
 
 @app.get('/registered/<slug>')
