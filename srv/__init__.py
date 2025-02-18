6bffdd386172899cf445a9b12eac0ac5f135cce4
rho <rho.rhoit@gmail.com>
2025-02-18 16:52:06 +0545

    srv - __init__.py CustomJSONEncoder stop sorting json

--- srv/__init__.py
+++ srv/__init__.py
@@ -7,3 +7,10 @@ app = flask.Flask(__name__)
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 app.jinja_env.undefined = jinja2.StrictUndefined
+
+
+class CustomJSONEncoder(flask.json.provider.DefaultJSONProvider):
+    sort_keys = False
+
+
+app.json = CustomJSONEncoder(app)
