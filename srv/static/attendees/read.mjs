182fac0f00bfd070f89d9f81a648216824b1595f
rughimire <rughimire@gmail.com>
2025-02-19 14:08:48 +0545

    attendees - read.mjs generic message for payment receipt file download box

--- srv/static/attendees/read.mjs
+++ srv/static/attendees/read.mjs
@@ -3,10 +3,9 @@ export function checkPaymentReceipt(slug) {
         `/registered/payment_receipt_check/${slug}`
     ).then(function (response) {
         if(response.status == 200) {
-            // console.log("Found")
-            document.getElementById('exising-receipt').innerHTML = 
+            document.getElementById('exising-receipt').innerHTML =
                 "<div class='alert alert-info'>"+
-                "You have already uploaded a document. &#11147;"+
+                "Payment receipt file  &#11147;"+
                 " <a href='/registered/payment_receipt_download/" + slug + "'>Download</a>"+
                 "</div>"
             document.getElementById('submit-receiptFile').innerText = "Update receipt"
