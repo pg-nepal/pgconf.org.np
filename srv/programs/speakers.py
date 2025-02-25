d4724e037ae5856e3776cf36a0a57a94a7055e61
rho <rho.rhoit@gmail.com>
2025-02-25 11:34:34 +0545

    programs - speakers.py get photo

--- srv/programs/speakers.py
+++ srv/programs/speakers.py
@@ -1,3 +1,5 @@
+import io
+
 import flask
 import sqlalchemy as sa
 
@@ -6,6 +8,13 @@ import db.events
 import db.programs
 
 from srv import app
+
+
+FILE_MAGIC_NUMBERS = {
+    b'%PDF-'             : ('application/pdf', 'pdf'),
+    b'\xFF\xD8\xFF'      : ('image/jpeg', 'jpg'),
+    b'\x89PNG\r\n\x1a\n' : ('image/png', 'png'),
+}
 
 
 @app.route('/programs/speakers')
@@ -35,3 +44,30 @@ def programs_speaker_list_page():
         baseURL   = '/speakers',
         cursor    = cursor,
     )
+
+
+@app.route('/programs/speakers/<name>')
+def programs_speaker_photoBlob(name):
+    query = sa.select(
+        db.events.Attendee.photoBlob,
+    ).where(
+        db.events.Attendee.name == name,
+    )
+
+    with db.engine.connect() as connection:
+        blob = connection.execute(query).scalar()
+
+        if blob is None:
+            return 'File Not Found', 404
+
+        mimetype, ext = 'application/octet-stream', ''
+        for magic, mime in FILE_MAGIC_NUMBERS.items():
+            if not blob.startswith(magic): continue
+            mimetype, ext = mime
+
+        slug = name.lower().replace(' ', '-')
+        return flask.send_file(
+            io.BytesIO(blob),
+            download_name = '{}.{}'.format(slug, ext),
+            mimetype      = mimetype,
+        )
