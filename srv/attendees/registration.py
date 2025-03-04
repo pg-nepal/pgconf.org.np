2eaac0cce1114f8491e2edbd0d74f6ac52fd8041
xebster <prabhatst18@gmail.com>
2025-03-04 10:31:11 +0545

    attendees - registration.py Added method to add event in tickets table

--- srv/attendees/registration.py
+++ srv/attendees/registration.py
@@ -276,6 +276,7 @@ def registered_ticket_read(slug):
         db.conf.Ticket.currency.label('Currency'),
         db.conf.Ticket.fee.label('Amount'),
         db.conf.Ticket.paymentStatus.label('Payment Status'),
+        db.conf.Ticket.attendee_pk.label('Attendee_pk'),
     ).outerjoin(
         db.conf.Ticket,
         sa.and_(
@@ -285,8 +286,56 @@ def registered_ticket_read(slug):
     )
 
     with db.engine.connect() as connection:
+        attendee_cursor = connection.execute(query)
+
+        for row in attendee_cursor:
+            if(row[4] is not None):
+                attendee_pk = row[4]
+
         cursor = connection.execute(query)
+
         return flask.jsonify(
             headers = tuple(c['name'] for c in query.column_descriptions),
             data    = [list(row) for row in cursor],
+            attendee_pk = attendee_pk,
         )
+
+
+@app.post('/registered/addevent')
+def registered_add_event():
+    jsonData = flask.request.json
+
+    for item in jsonData['data']:
+        if(item[1] is None):
+            query = sa.select(
+                db.conf.Event.pk,
+            ).where(
+                db.conf.Event.name == item[0],
+            )
+
+            with db.engine.connect() as connection:
+                cursor = connection.execute(query).first()
+                events = str(cursor[0])
+
+                attendee_cursor = connection.execute(sa.select(
+                            db.conf.Attendee.pk,
+                            db.conf.Attendee.slug,
+                            db.conf.Attendee.email,
+                            db.conf.Attendee.country,
+                            db.conf.Attendee.category,
+                        ).where(
+                            db.conf.Attendee.pk == jsonData['attendee_pk'],
+                        ))
+
+                attendee = attendee_cursor.first()
+
+                for row in cursor:
+                    connection.execute(sa.insert(
+                        db.conf.Ticket,
+                    ).values(
+                        tickets_generate(attendee, events),
+                    ))
+
+                    connection.commit()
+
+    return(jsonData)
