797ba7c312db79739cf853173d806123b0e2e153
rho <rho.rhoit@gmail.com>
2025-03-06 12:56:14 +0545

    initdb.py events data

--- initdb.py
+++ initdb.py
@@ -1,6 +1,9 @@
 #!venv/bin/python
 
+import datetime as dt
+
 import sqlalchemy as sa
+import sqlalchemy.dialects.postgresql
 
 import db
 import db.mbox
@@ -14,6 +17,25 @@ def main(schemas):
             conn.execute(sa.text('CREATE SCHEMA IF NOT EXISTS {}'.format(schema))) # noqa: E501
 
     db.meta.create_all(bind=db.engine, checkfirst=True)
+
+    with db.engine.begin() as conn:
+        conn.execute(sa.dialects.postgresql.insert(
+            db.conf.Event,
+        ).values([
+            {
+                'pk'      : 1,
+                'name'    : 'Pre Conference Traning',
+                'eventOn' : dt.datetime(2025, 5, 3),
+                'eventTo' : dt.datetime(2025, 5, 4),
+            },
+
+            {
+                'pk'      : 2,
+                'name'    : 'Main Conference',
+                'eventOn' : dt.datetime(2025, 5, 5),
+                'eventTo' : dt.datetime(2025, 5, 6),
+            },
+        ]).on_conflict_do_nothing())
 
 
 if __name__ == '__main__':
