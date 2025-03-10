78d786d3c0547eea09fb4b9c1db7efd18d7fdcb7
xebster <prabhatst18@gmail.com>
2025-03-10 15:42:46 +0545

    tickets - routes.py

---
+++ srv/tickets/routes.py
@@ -0,0 +1,36 @@
+import flask
+import sqlalchemy as sa
+import sqlalchemy.dialects.postgresql
+
+import db
+import db.conf
+
+from srv import app
+
+@app.get('/tickets/<slug>')
+def ticket_read(slug):
+    query = sa.select(
+        db.conf.Event.pk.label('pk'),
+        db.conf.Event.name.label('Event'),
+        db.conf.Event.eventOn.label('Date From'),
+        db.conf.Event.eventTo.label('Date To'),
+        db.conf.Ticket.currency.label('Currency'),
+        db.conf.Ticket.fee.label('Amount'),
+        db.conf.Ticket.paymentStatus.label('Payment Status'),
+        db.conf.Ticket.createdOn.label('Ordered Date'),
+        db.conf.Ticket.updatedOn.label('Updated Date'),
+    ).outerjoin(
+        db.conf.Ticket,
+        sa.and_(
+            db.conf.Ticket.event_pk == db.conf.Event.pk,
+            sa.cast(db.conf.Ticket.attendee_slug, sa.String) == slug,
+        ),
+    ).order_by(db.conf.Event.pk)
+
+    with db.engine.connect() as connection:
+        cursor = connection.execute(query)
+
+        return flask.render_template(
+            '/parts/ticket.djhtml',
+            cursor = cursor,
+        )
