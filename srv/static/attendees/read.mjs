74dfadef20f2e5818f2343ec0404fcf61f2573f7
xebster <prabhatst18@gmail.com>
2025-03-11 10:24:39 +0545

    attendees - read.mjs TicketDetails api changed

--- srv/static/attendees/read.mjs
+++ srv/static/attendees/read.mjs
@@ -1,6 +1,6 @@
 
 export function getTicketDetails(slug) {
-    fetch(`/tickets/${slug}`).then(function (response) {
+    fetch(`/ticket/${slug}`).then(function (response) {
         if (200 == response.status) {
             response.text()
             .then(function(data) {
