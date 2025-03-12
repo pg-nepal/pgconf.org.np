e10d0b6a94cd4b15cfe240b45f4b5ca839a0d808
xebster <prabhatst18@gmail.com>
2025-03-12 10:00:56 +0545

    attendees - crud.py UPDATE

--- srv/attendees/crud.py
+++ srv/attendees/crud.py
@@ -76,3 +76,23 @@ def attendee_read(pk):
                 'Category'      : row.category,
             },
         )
+
+
+@app.post('/attendees/<int:pk>')
+@srv.auth.auth_required()
+def attendee_update(pk):
+    file_photoBlob = flask.request.files['photoBlob']
+
+    if file_photoBlob is None:
+        return 'File not uploaded', 400
+
+    with db.SessionMaker.begin() as session:
+        cursor = session.execute(sa.update(
+            db.conf.Attendee,
+        ).where(
+            db.conf.Attendee.pk == pk,
+        ).values(
+            photoBlob     = file_photoBlob.read(),
+            photoMime     = file_photoBlob.content_type,
+        ))
+        return 'Updated Rows', 202 if cursor.rowcount > 0 else 400
