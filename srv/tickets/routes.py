628dac68ff477b7163a590305d0e9ef0acfbfaf6
xebster <prabhatst18@gmail.com>
2025-03-16 09:49:42 +0545

    tickets - routes.py - Receipt_read api added

--- srv/tickets/routes.py
+++ srv/tickets/routes.py
@@ -34,3 +34,28 @@ def ticket_read(slug):
             '/parts/ticket.djhtml',
             cursor = cursor,
         )
+
+
+@app.get('/receipts/<slug>')
+def receipt_read(slug):
+    query = sa.select(
+        db.conf.Event.pk.label('pk'),
+        db.conf.Event.name.label('Event'),
+        db.conf.Ticket.currency.label('Currency'),
+        db.conf.Ticket.fee.label('Amount'),
+        db.conf.Ticket.paymentStatus.label('Payment Status'),
+    ).join(
+        db.conf.Ticket,
+        sa.and_(
+            db.conf.Ticket.event_pk == db.conf.Event.pk,
+            sa.cast(db.conf.Ticket.attendee_slug, sa.String) == slug,
+        ),
+    ).order_by(db.conf.Event.pk)
+
+    with db.engine.connect() as connection:
+        cursor = connection.execute(query)
+
+        return flask.render_template(
+            '/parts/receipt.djhtml',
+            tickets = [row._asdict() for row in cursor],
+        )
