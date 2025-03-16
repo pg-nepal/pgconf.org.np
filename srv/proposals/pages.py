89338f03fadfbfdf78ad9403c74583cbe2e4ee68
xebster <prabhatst18@gmail.com>
2025-03-16 17:27:33 +0545

    proposals - pages.py - Accept proposal api created

--- srv/proposals/pages.py
+++ srv/proposals/pages.py
@@ -1,5 +1,8 @@
 import flask
+import sqlalchemy as sa
 
+import db
+import db.programs
 import srv.auth
 
 from srv import app
@@ -16,3 +19,67 @@ def proposal_read_view(pk):
         isAdmin = isAdmin,
         pk      = pk,
     )
+
+
+@app.get('/proposals/accept')
+def proposal_accept_view():
+    isAdmin = srv.auth.isValid(flask.request)
+    if isAdmin is False:
+        return srv.auth.respondInValid()
+
+    return flask.render_template(
+        '/proposals/accept.djhtml',
+        pageTitle = 'Accept Proposals',
+        pageDesc  = 'List of all submitted proposals',
+        baseURL   = '/proposals/accept',
+        isAdmin   = isAdmin,
+    )
+
+
+@app.get('/api/proposals/accept')
+def proposal_accept_list_api():
+    isAdmin = srv.auth.isValid(flask.request)
+    if isAdmin is False:
+        return srv.auth.respondInValid()
+
+    query = sa.select(
+        db.programs.Proposal.pk,
+        db.programs.Proposal.title,
+        sa.func.coalesce(sa.func.round(sa.func.avg(db.programs.Rate.value), 0)).label('avg(rating)'),
+        sa.literal(None).label('Action'),
+    ).outerjoin(
+        db.programs.Rate,
+        db.programs.Proposal.pk == db.programs.Rate.proposal_pk,
+    ).group_by(
+        db.programs.Proposal.pk,
+    ).order_by(
+        db.programs.Proposal.pk,
+    )
+
+    with db.engine.connect() as connection:
+        cursor = connection.execute(query)
+
+        return flask.jsonify(
+            headers = tuple(c['name'] for c in query.column_descriptions),
+            data    = [ list(row) for row in cursor ],
+        )
+
+
+@app.get('/proposals/accept/<int:pk>')
+def proposal_accept(pk):
+    isAdmin = srv.auth.isValid(flask.request)
+    if isAdmin is False:
+        return srv.auth.respondInValid()
+
+    query = sa.update(
+        db.programs.Proposal,
+    ).where(
+        db.programs.Proposal.pk == pk,
+    ).values(
+        status      = 'accepted',
+        updatedBy   = isAdmin,
+    )
+
+    with db.SessionMaker.begin() as session:
+        session.execute(query)
+        return "Proposal accepted"
