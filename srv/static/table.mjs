005039191c0d344caed209c09f45ecfa973a2833
rho <rho.rhoit@gmail.com>
2025-03-19 09:58:57 +0545

    table.mjs - filter get current filter

--- srv/static/table.mjs
+++ srv/static/table.mjs
@@ -92,6 +92,7 @@ export function load(id, baseURL) {
         return response.json()
     }).then(function (json) {
         const eTable = document.getElementById(id)
+        const searchParams = new URLSearchParams(location.search)
 
         for (const h of json.headers) {
             const eTh = document.createElement('th')
@@ -113,6 +114,7 @@ export function load(id, baseURL) {
 
                     for (let o of json.filters[h]) {
                         const eOption = document.createElement('option')
+                        eOption.selected = (o == searchParams.get(h))
                         eOption.innerText = o
                         eSelect.appendChild(eOption)
                     }
