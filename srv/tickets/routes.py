d08e96a47a67c82e84b8bb3ab7d398453f61c3f6
rho <rho.rhoit@gmail.com>
2025-03-21 10:37:15 +0545

    tickets - routes.py /tickets/<slug>

--- srv/tickets/routes.py
+++ srv/tickets/routes.py
@@ -7,9 +7,39 @@ import db.conf
 from srv import app
 
 
+@app.get('/ticket/<slug>')
+def ticket_show_mine(slug):
+    query = sa.select(
+        db.conf.Event.pk,
+        db.conf.Event.name,
+        db.conf.Event.eventOn,
+        db.conf.Event.eventTo,
+        db.conf.Ticket.status,
+        db.conf.Ticket.currency,
+        db.conf.Ticket.fee,
+        db.conf.Ticket.paymentStatus,
+        db.conf.Ticket.createdOn,
+        db.conf.Ticket.updatedOn,
+    ).outerjoin(
+        db.conf.Ticket,
+        sa.and_(
+            db.conf.Ticket.event_pk == db.conf.Event.pk,
+            db.conf.Ticket.status   == 'booked',
+            sa.cast(db.conf.Ticket.attendee_slug, sa.String) == slug,
+        ),
+    ).order_by(db.conf.Event.pk)
+
+    with db.engine.connect() as connection:
+        cursor = connection.execute(query)
+
+        return flask.render_template(
+            '/parts/ticket.djhtml',
+            cursor = cursor,
+        )
+
 
 @app.get('/tickets/receipt/<slug>')
-def registered_receipt_read(slug):
+def receipt_read(slug):
     query = sa.select(
         db.conf.Event.pk.label('pk'),
         db.conf.Event.name.label('Event'),
