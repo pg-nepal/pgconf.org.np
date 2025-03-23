ff6bbea92f3aeb46ab2a8639cb1d25ad33875b86
chimpKiller <shashankshrestha21@gmail.com>
2025-03-23 12:11:32 +0545

    tickets - routes.py new route for showing receipt table

--- srv/tickets/routes.py
+++ srv/tickets/routes.py
@@ -66,3 +66,45 @@ def receipt_read(slug):
             headers = tuple(c['name'] for c in query.column_descriptions),
             data    = [list(row) for row in cursor],
         )
+
+
+@app.get('/registered/tickets/receipt/<slug>')
+def receipt_read_client(slug):
+    query = sa.select(
+        db.conf.Event.name.label('Event'),
+        db.conf.Ticket.currency.label('Currency'),
+        db.conf.Ticket.fee.label('Amount'),
+        sa.cast(db.conf.Ticket.status.label('Ticket Status'), sa.String),
+        db.conf.Ticket.paymentStatus.label('Payment Status'),
+        db.conf.Ticket.createdOn.label('Ordered Date'),
+        sa.literal('').label('Action'),
+    ).outerjoin(
+        db.conf.Ticket,
+        sa.and_(
+            db.conf.Ticket.event_pk == db.conf.Event.pk,
+            db.conf.Ticket.status   != 'cancelled',
+            sa.cast(db.conf.Ticket.attendee_slug, sa.String) == slug,
+        ),
+    ).order_by(db.conf.Event.pk)
+
+    pk_query = sa.select(
+        db.conf.Event.pk.label('pk'),
+        db.conf.Event.name.label('Name'),
+    ).outerjoin(
+        db.conf.Ticket,
+        sa.and_(
+            db.conf.Ticket.event_pk == db.conf.Event.pk,
+            db.conf.Ticket.status   != 'cancelled',
+            sa.cast(db.conf.Ticket.attendee_slug, sa.String) == slug,
+        ),
+    ).order_by(db.conf.Event.pk)
+
+    with db.engine.connect() as connection:
+        cursor = connection.execute(query)
+        ccursor = connection.execute(pk_query)
+
+        return flask.jsonify(
+            headers = tuple(c['name'] for c in query.column_descriptions),
+            data    = [row._asdict() for row in cursor],
+            pk      = [row._asdict() for row in ccursor],
+        )
