cc9cb3dc0c790f1c42bef339277c14c3bba582d7
chimpKiller <shashankshrestha21@gmail.com>
2025-03-30 10:46:43 +0545

    proposals - pages.py api for evaluation table

--- srv/proposals/pages.py
+++ srv/proposals/pages.py
@@ -83,6 +83,51 @@ def proposal_evaluation_list_api():
         )
 
 
+
+@app.get('/api/proposals/evaluation/<slug>')
+def submitted_evaluation_mine(slug):
+
+    query = sa.select(
+        db.programs.Proposal.title,
+        sa.func.coalesce(sa.func.round(sa.func.avg(db.programs.Rate.value), 0)).label('avg(rating)'),  # noqa:E501
+        sa.func.coalesce(sa.func.round(sa.func.avg(sa.func.cast(sa.func.json_extract_path_text(db.programs.Rate.score, 'relevance'), sa.Integer)), 2)).label('Relevance'),  # noqa:E501
+        sa.func.coalesce(sa.func.round(sa.func.avg(sa.func.cast(sa.func.json_extract_path_text(db.programs.Rate.score, 'clarity'), sa.Integer)), 2)).label('Clarity'),  # noqa:E501
+        sa.func.coalesce(sa.func.round(sa.func.avg(sa.func.cast(sa.func.json_extract_path_text(db.programs.Rate.score, 'engagement'), sa.Integer)), 2)).label('Engagement'),  # noqa:E501
+        sa.func.coalesce(sa.func.round(sa.func.avg(sa.func.cast(sa.func.json_extract_path_text(db.programs.Rate.score, 'content'), sa.Integer)), 2)).label('Content'),  # noqa:E501
+        sa.func.count(db.programs.Rate.proposal_pk).label('Rated by'),
+        sa.cast(db.programs.Proposal.status, sa.String),
+    ).outerjoin(
+        db.programs.Rate,
+        db.programs.Proposal.pk == db.programs.Rate.proposal_pk,
+    ).where(
+        db.programs.Proposal.slug == slug,
+    ).group_by(
+        db.programs.Proposal.pk,
+    ).order_by(
+        db.programs.Proposal.pk,
+    )
+
+    with db.engine.connect() as connection:
+        cursor = connection.execute(query)
+        row = cursor.first()
+
+        if row is None:
+            return 'Invalid proposal', 400
+
+        return flask.jsonify(
+            data  = {
+                'Title'     : row.title,
+                'Avg Rating': row._mapping['avg(rating)'],
+                'Relevance' : row.Relevance,
+                'Clarity'   : row.Clarity,
+                'Engagement': row.Engagement,
+                'Content'   : row.Content,
+                'Rated by'  : row._mapping['Rated by'],
+                'Status'    : row.status,
+            }, 
+        )
+
+
 @app.post('/proposals/evaluation/<int:pk>')
 def proposal_evaluation(pk):
     isAdmin = srv.auth.isValid(flask.request)
