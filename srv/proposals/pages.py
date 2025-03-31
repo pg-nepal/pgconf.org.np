f9e2ba6f22ef7c03f8dd05ef93147f7beb826523
chimpKiller <shashankshrestha21@gmail.com>
2025-03-31 16:23:50 +0545

    proposal - pages.py evaluation list with ratings

--- srv/proposals/pages.py
+++ srv/proposals/pages.py
@@ -22,23 +22,8 @@ def proposal_read_view(pk):
 
 
 @app.get('/proposals/evaluation')
-def proposal_evaluation_view():
-    isAdmin = srv.auth.isValid(flask.request)
-    if isAdmin is False:
-        return srv.auth.respondInValid()
+def evaluation_list():
 
-    return flask.render_template(
-        '/table.djhtml',
-        pageTitle   = 'Evaluate Proposals',
-        pageDesc    = 'List of all submitted proposals',
-        baseURL     = '/proposals',
-        extendedURL = '/evaluation',
-        isAdmin     = isAdmin,
-    )
-
-
-@app.post('/api/proposals/evaluation')
-def proposal_evaluation_list_api():
     isAdmin = srv.auth.isValid(flask.request)
     if isAdmin is False:
         return srv.auth.respondInValid()
@@ -48,38 +33,22 @@ def proposal_evaluation_list_api():
     query = sa.select(
         db.programs.Proposal.pk,
         db.programs.Proposal.title,
-        sa.func.coalesce(sa.func.round(sa.func.avg(db.programs.Rate.value), 0)).label('avg(rating)'),  # noqa:E501
-        sa.func.coalesce(sa.func.round(sa.func.avg(sa.func.cast(sa.func.json_extract_path_text(db.programs.Rate.score, 'relevance'), sa.Integer)), 2)).label('Relevance'),  # noqa:E501
-        sa.func.coalesce(sa.func.round(sa.func.avg(sa.func.cast(sa.func.json_extract_path_text(db.programs.Rate.score, 'clarity'), sa.Integer)), 2)).label('Clarity'),  # noqa:E501
-        sa.func.coalesce(sa.func.round(sa.func.avg(sa.func.cast(sa.func.json_extract_path_text(db.programs.Rate.score, 'engagement'), sa.Integer)), 2)).label('Engagement'),  # noqa:E501
-        sa.func.coalesce(sa.func.round(sa.func.avg(sa.func.cast(sa.func.json_extract_path_text(db.programs.Rate.score, 'content'), sa.Integer)), 2)).label('Content'),  # noqa:E501
-        sa.func.count(db.programs.Rate.proposal_pk).label('Rated by'),
-        sa.cast(db.programs.Proposal.status, sa.String),
-        db.programs.Proposal.status.label('Action'),
-    ).outerjoin(
+        db.programs.Proposal.abstract,
+        sa.func.coalesce(sa.func.round(sa.func.avg(db.programs.Rate.value), 0)).label('Average Rating'),
+    ).join(
         db.programs.Rate,
         db.programs.Proposal.pk == db.programs.Rate.proposal_pk,
     ).group_by(
         db.programs.Proposal.pk,
+        db.programs.Proposal.title,
+        db.programs.Proposal.abstract,
     ).order_by(
         db.programs.Proposal.pk,
     )
 
     with db.engine.connect() as connection:
-        cursor = connection.execute(query.where(*[
-            c['expr'] == jsonData['filter'][c['name']]
-            for c in query.column_descriptions
-            if jsonData['filter'].get(c['name'], 'all') != 'all'
-        ]).order_by(
-            db.programs.Proposal.pk,
-        ))
-
-        return flask.jsonify(
-            headers = tuple(c['name'] for c in query.column_descriptions),
-            data    = [ list(row) for row in cursor ],
-            filters = {
-                'status' : ('all', *db.programs.proposal_status),
-            },
+        return flask.render_template(
+            '/proposals/evaluation.djhtml',
         )
 
 
