59fa41e49c9aee964f1b0610fc620e44e53796f3
chimpKiller <shashankshrestha21@gmail.com>
2025-04-10 15:21:33 +0545

    talk - schedule.py loading the json file and returning it

---
+++ srv/talk/schedule.py
@@ -0,0 +1,18 @@
+import sys
+import json
+import pathlib
+import flask
+
+from srv import app
+
+FILE = pathlib.Path('schedule.json').resolve()
+
+if FILE.exists():
+    print('# Loading schedule from: {}'.format(FILE), file=sys.stderr)
+    with FILE.open() as fp:
+        schedule = json.load(fp)
+
+        
+@app.get('/schedule_data')
+def schedule_data():
+    return flask.jsonify(schedule)        
