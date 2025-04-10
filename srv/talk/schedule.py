094c76eed002319860339773c6cb728989f4d554
chimpKiller <shashankshrestha21@gmail.com>
2025-04-10 17:20:56 +0545

    talk - schedule.py removed import for sys as not used

--- srv/talk/schedule.py
+++ srv/talk/schedule.py
@@ -1,4 +1,3 @@
-import sys
 import json
 import pathlib
 import flask
@@ -6,11 +5,6 @@ import flask
 from srv import app
 
 FILE = pathlib.Path('schedule.json').resolve()
-
-if FILE.exists():
-    print('# Loading schedule from: {}'.format(FILE), file=sys.stderr)
-    with FILE.open() as fp:
-        schedule = json.load(fp)
 
         
 @app.get('/schedule_data')
