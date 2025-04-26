4869b29db6cb8d5e6a9dc1601cb73706252edf66
rughimire <rughimire@gmail.com>
2025-04-26 13:12:58 +0545

    dash - pass ticket and payment status

--- srv/dash/route.py
+++ srv/dash/route.py
@@ -1,19 +1,18 @@
 import flask
 
 import srv.auth
-
+import srv.dash.reports
 from srv import app
 
 
 @app.get('/dash')
 @app.get('/admin')
 @app.get('/organizer')
+@srv.auth.auth_required()
 def dash_index():
-    isAdmin = srv.auth.isValid(flask.request)
-    if isAdmin is False:
-        return srv.auth.respondInValid()
-
     return flask.render_template(
         '/dash/main.djhtml',
-        isAdmin = isAdmin,
+        isAdmin = srv.auth.loggedInUser(flask.request),
+        registration_tickets_status = srv.dash.reports.get_registration_tickets_status(),
+        registration_payment_status = srv.dash.reports.get_registration_payment_status(),
     )
