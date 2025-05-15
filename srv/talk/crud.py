236d06f23c05a7aa23a341b1473748bbe1d6b842
chimpKiller <shashankshrestha21@gmail.com>
2025-05-15 15:32:35 +0545

    talk - crud.py route for listing talks and reading talks

---
+++ srv/talk/crud.py
@@ -0,0 +1,57 @@
+import flask
+import sqlalchemy as sa
+
+import db
+import db.conf
+
+from srv import app
+
+
+@app.get('/talks')
+def talks_list():
+    query = sa.select(
+        db.programs.Proposal.slug,
+        db.programs.Proposal.name,
+        db.programs.Proposal.session,
+        db.programs.Proposal.title,
+    ).where(
+        db.programs.Proposal.status == 'accepted',
+    )
+
+    with db.engine.connect() as connection:
+        cursor = connection.execute(query)
+        return flask.jsonify(
+            [row._asdict() for row in cursor],
+        )
+
+
+@app.post('/talks')
+def talk_read():
+    slug = flask.request.form.get('slug')
+
+    query = sa.select(
+        db.programs.Proposal.name,
+        db.programs.Proposal.session,
+        db.programs.Proposal.title,
+        db.programs.Proposal.abstract,
+        db.programs.Proposal.attendee_pk,
+        db.programs.Proposal.slideBlob,
+
+        db.conf.Attendee.pk,
+        db.conf.Attendee.bio,
+        db.conf.Attendee.affiliation,
+    ).outerjoin(
+        db.conf.Attendee,
+        db.programs.Proposal.attendee_pk == db.conf.Attendee.pk,
+    ).where(
+        db.programs.Proposal.slug == slug,
+        db.programs.Proposal.status == 'accepted',
+    )
+
+    with db.engine.connect() as connection:
+        cursor = connection.execute(query)
+        row = cursor.first()
+        return flask.render_template(
+            '/parts/talk-read.djhtml',
+            row = row,
+        )
